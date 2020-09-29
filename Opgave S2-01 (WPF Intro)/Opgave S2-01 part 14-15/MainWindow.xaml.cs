3c71b284d5c707e090b04f46aac9f678bca0292c
Lucas Kyhl <luca609a@edu.campusvejle.dk>
2020-09-29 13:27:53 +0200

    New assignment

--- Opgave S2-01 (WPF Intro)/Opgave S2-01 part 14-15/MainWindow.xaml.cs
+++ Opgave S2-01 (WPF Intro)/Opgave S2-01 part 14-15/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,6 +21,42 @@ namespace Opgave_S2_01_part_14_15 {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+        }
+
+        private void RadRed_Checked(object sender, RoutedEventArgs e) {
+            mainWindow.Background = new SolidColorBrush(Color.FromArgb(255, 255, 0, 0));
+        }
+
+        private void RedYellow_Checked(object sender, RoutedEventArgs e) {
+            mainWindow.Background = new SolidColorBrush(Color.FromArgb(255, 255, 255, 0));
+        }
+
+        private void RedGreen_Checked(object sender, RoutedEventArgs e) {
+            mainWindow.Background = new SolidColorBrush(Color.FromArgb(255, 0, 255, 0));
+        }
+
+        private void RadHide_Checked(object sender, RoutedEventArgs e) {
+            radRed.Visibility = Visibility.Hidden;
+            radYellow.Visibility = Visibility.Hidden;
+            radGreen.Visibility = Visibility.Hidden;
+        }
+
+        private void RadInactive_Checked(object sender, RoutedEventArgs e) {
+            radRed.IsEnabled = false;
+            radYellow.IsEnabled = false;
+            radGreen.IsEnabled = false;
+        }
+
+        private void RadActive_Checked(object sender, RoutedEventArgs e) {
+            radRed.IsEnabled = true;
+            radYellow.IsEnabled = true;
+            radGreen.IsEnabled = true;
+        }
+
+        private void RadHide_Unchecked(object sender, RoutedEventArgs e) {
+            radRed.Visibility = Visibility.Visible;
+            radYellow.Visibility = Visibility.Visible;
+            radGreen.Visibility = Visibility.Visible;
         }
     }
 }
---
+++ Opgave S2-03 WPF Gaming 1 (Rock Paper Scissors)/Opgave S2-03 WPF Gaming 1 (Rock Paper Scissors)/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Opgave_S2_03_WPF_Gaming_1__Rock_Paper_Scissors_ {
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application {
+    }
+}
