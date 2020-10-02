fe68484d1c451f0c3943f76a14ea065dbe36d19a
LucasAspIT <luca609a@edu.campusvejle.dk>
2020-10-02 17:42:52 +0200

    Added new assignment

---
+++ Opgave S2-04 WPF Gaming 2 (Catch falling stuff)/Opgave S2-04 WPF Gaming 2 (Catch falling stuff)/Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Opgave_S2_04_WPF_Gaming_2__Catch_falling_stuff_ {
+    class Player {
+
+        private void OnButtonKeyDown(object sender, KeyEventArgs e) {
+            switch (e.Key) {
+                case Key.Right:
+                    // if (previousDirection != (int)MovingDirection.Left)
+                    direction = (int)MovingDirection.Right;
+                    break;
+                case Key.Left:
+                    // if (previousDirection != (int)MovingDirection.Right)
+                    direction = (int)MovingDirection.Left;
+                    break;
+            }
+        }
+    }
+}
